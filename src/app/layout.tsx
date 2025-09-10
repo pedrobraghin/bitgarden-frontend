65a9d715e7b95705a48dcfdd54e8ad424d8d63c9
Pedro Braghin <braghindev@gmail.com>
2025-09-10 17:08:23 -0300

    feat(loader): add loader between page transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'BitGarden',
-  description: 'Sua plataforma de contribuição para projetos de TI.',
+  title: "BitGarden",
+  description: "Sua plataforma de contribuição para projetos de TI.",
 };
 
 export default function RootLayout({
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen bg-black text-white`}
       >
+        <NextTopLoader showSpinner={false} />
         {children}
       </body>
     </html>
